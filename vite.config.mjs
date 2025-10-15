caeeea94760f2831a03459e350c3a4ab8f520216
Martin Roos <roosmartin67@proton.me>
2025-10-15 22:56:46 +0200

    Rename vite.config.js to vite.config.mjs for Netlify ESM

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',       // should point to project root where index.html is
   build: {
-    outDir: 'dist' // matches Netlify publish directory
+    outDir: 'dist'
   }
 })
